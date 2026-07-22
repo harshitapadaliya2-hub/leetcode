11ec5aef9b6353b6324b27870aa4ee60ea5d7cb0
Harshita Padaliya <harshitapadaliya2@gmail.com>
2026-07-22 22:05:42 +0530

    Time: 20 ms (37.08%) | Memory: 24.9 MB (80.12%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>>mp;
+        for (string str:strs) {
+             string key =str;
+             sort(key.begin(),key.end());
+            mp[key].push_back(str);
+        }
+        vector<vector<string>>ans;
+        for (auto& i:mp) {
+            ans.push_back(i.second);
+        }
+        return ans;
+    }
+};

f1b082b31789182be18ba601591a5d643440db27
Harshita Padaliya <harshitapadaliya2@gmail.com>
2026-08-21 22:42:07 +0530

    Time: 37 ms (96.28%) | Memory: 72.2 MB (59.85%) - LeetSync

---
+++ 4034-maximum-gap-between-stations/maximum-gap-between-stations.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int maximumGap(string skill, string station) {
+         int n = skill.size();
+        int m = station.size();
+        vector<int>l(n);
+         vector<int>r(n);
+        int j = 0;
+        for (int i = 0; i < n; i++) {
+            while (station[j] != skill[i]) {
+                j++;
+            }
+            l[i] = j;
+            j++;
+        }
+        j = m-1;
+        for (int i = n-1;i>= 0;i--) {
+            while (station[j]!=skill[i]) {
+                j--;
+            }
+            r[i]=j;
+            j--;
+        }
+        int ans = 0;
+        for (int i = 1;i<n;i++) {
+         ans = max(ans,r[i] -l[i -1]);
+        }
+        return ans;
+    }
+};
